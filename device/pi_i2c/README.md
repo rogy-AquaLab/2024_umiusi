a5430f1d5703ed1dbd8e7145751af34bbe907a6b
H1rono <hronok66@gmail.com>
2024-05-30 18:24:20 +0900

    Create package

---
+++ device/pi_i2c/README.md
@@ -0,0 +1,9 @@
+# nucleo communication
+
+Raspberry PiのI2C接続
+
+## Nodes
+
+## Executables
+
+## Launches
---
+++ device/pi_i2c/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'pi_i2c'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='h1rono',
+    maintainer_email='hronok66@gmail.com',
+    description='Raspberry PiのI2C接続',
+    license='MIT',
+    tests_require=[],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
