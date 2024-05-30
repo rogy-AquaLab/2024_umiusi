a60905d64b569b8db9d91fd26f9da552f6a96b6e
H1rono <hronok66@gmail.com>
2024-05-30 19:05:22 +0900

    Add launch files

--- device/pi_i2c/README.md
+++ device/pi_i2c/README.md
@@ -14,3 +14,7 @@ Raspberry PiのI2C接続
 - `all`: `Depth`, `Imu`Node2つを同時にspin
 
 ## Launches
+
+- `all_launch.py`: `all`executableを`device`namespace下で実行
+- `depth_launch.py`: `depth`executableを`device`namespace下で実行
+- `imu_launch.py`: `imu`executableを`device`namespace下で実行
---
+++ device/pi_i2c/launch/depth_launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description() -> LaunchDescription:
+    depth = Node(
+        package="pi_i2c",
+        executable="depth",
+        namespace="device"
+    )
+    return LaunchDescription([depth])
