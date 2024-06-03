e399d9a3578848359fae0ae0ba91f38fe0012cce
H1rono <hronok66@gmail.com>
2024-06-03 23:53:31 +0900

    Write conditioning

--- app/simple_joy_app/src/app.cpp
+++ app/simple_joy_app/src/app.cpp
@@ -1,15 +1,44 @@
+#include <cmath>
 #include <functional>
 
 #include "rclcpp_components/register_node_macro.hpp"
-#include <rclcpp/logging.hpp>
-#include <sensor_msgs/msg/detail/joy__struct.hpp>
 
 #include "simple_joy_app/app.hpp"
 
 void app::App::joy_callback(const sensor_msgs::msg::Joy& msg) {
     RCLCPP_INFO_STREAM(this->get_logger(), "received joystick msg");
     // https://www.pygame.org/docs/ref/joystick.html#playstation-4-controller-pygame-2-x
-    // TODO
+
+    // スティックの値を見る最小範囲
+    constexpr double stick_threshold = 0.1;
+    // 左スティック h:水平 v:垂直
+    const double lstick_h = msg.axes[0];
+    const double lstick_v = -msg.axes[1]; // 上方向が負のため逆転
+    // 右スティック h:水平 v:垂直
+    const double rstick_h = msg.axes[2];
+    const double rstick_v = -msg.axes[3];
+
+    const bool lstick_h_effective = std::abs(lstick_h) > stick_threshold;
+    const bool lstick_v_effective = std::abs(lstick_v) > stick_threshold;
+    const bool lstick_effective = lstick_h_effective || lstick_v_effective;
+    const bool rstick_h_effective = std::abs(rstick_h) > stick_threshold;
+    const bool rstick_v_effective = std::abs(rstick_v) > stick_threshold;
+    const bool rstick_effective = rstick_h_effective || rstick_v_effective;
+
+    if (lstick_effective) {
+        // TODO: 並行移動
+        return;
+    }
+    if (!rstick_effective) {
+        // TODO: 何もしない
+        return;
+    }
+    if (rstick_h_effective) {
+        // TODO: 水平回転
+        return;
+    }
+    // assert(rstick_v_effective); 自明
+    // TODO: 垂直方向の移動
 }
 
 app::App::App(const rclcpp::NodeOptions& options) :
