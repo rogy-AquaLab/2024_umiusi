1dc1e992e92af9facfc78977bb4f2428b5b5da0e
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-06-04 14:56:17 +0900

    naoshi

--- device/camera_reader/src/main.cpp
+++ device/camera_reader/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<CameraNode>();
+    auto node = std::make_shared<CameraReader>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
