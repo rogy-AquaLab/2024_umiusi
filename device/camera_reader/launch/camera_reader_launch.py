4070357e0c145f97e6e447455bc778ad4d3e1562
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-06-14 19:27:25 +0900

    launch

---
+++ device/camera_reader/launch/camera_reader_launch.py
@@ -0,0 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="camera_reader", 
+            executable="camera_reader", 
+            namespace="device"
+            remappings=[("/device/camera/image", "/packet/camera/image")])
+    ])
