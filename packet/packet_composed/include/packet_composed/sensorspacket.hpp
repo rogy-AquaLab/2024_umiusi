6cfe834c62dbcc5776281976a34deb2be5ce9e31
{Noir-swim} <{noriri.jwu@gmail.com}>
2024-06-26 00:11:13 +0900

    不要なファイル消しました。composed.cpp、sensorspacket.hpp,CMakeLists.txt,package.xmlを修正しました。

--- packet/packet_composed/include/packet_composed/sensorspacket.hpp
+++ packet/packet_composed/include/packet_composed/sensorspacket.hpp
@@ -7,11 +7,31 @@
 
 class Composed : public rclcpp::Node {
 private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _publisher;
+    rclcpp::Publisher<packet_interfaces::msg::Composed>::SharedPtr composed_publisher;
 
-    std::shared_ptr<rclcpp::Publisher<std_msgs::msg::String>> _publisher;
-    std::shared_ptr<rclcpp::TimerBase> _timer;
-    unsigned int _count;
+    rclcpp::Subscription<packet_interfaces::msg::Depth>::SharedPtr depth_subscription;
+    rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr         imu_subscription;
+    rclcpp::Subscription<packet_interfaces::msg::Flex>::SharedPtr  flex1_subscription;
+    rclcpp::Subscription<packet_interfaces::msg::Flex>::SharedPtr  flex2_subscription;
+    rclcpp::Subscription<packet_interfaces::msg::current>::SharedPtr  current_subscription;
+    rclcpp::Subscription<packet_interfaces::msg::voltage>::SharedPtr  voltage_subscription;
+
+    //まとめてpublishするためにタイマーのつもり
+    rclcpp::TimerBase::SharedPtr _timer;
+    size_t _count;
+    packet_interfaces::msg::Composed composed_msg;
+
     void _loop();
+    void depth_topic_callback(const packet_interfaces::msg::Depth& msg);
+    void imu_topic_callback(const sensor_msgs::msg::Imu& msg);
+    void flex1_topic_callback(const packet_interfaces::msg::Flex1& msg);
+    void flex2_topic_callback(const packet_interfaces::msg::Flex2& msg);
+    void current_topic_callback(const packet_interfaces::msg::current& msg);
+    void voltage_topic_callback(const packet_interfaces::msg::voltage& msg);
+ 
+
+    // さらにいくつかのメソッド, プロパティが必要になる
 
 public:
     Composed();
--- packet/packet_composed/src/composed.cpp
+++ packet/packet_composed/src/composed.cpp
@@ -1,8 +1,41 @@
 #include <chrono>
 #include <functional>
 #include <string>
-
 #include "packet_composed/sensorspacket.hpp"
+
+
+Composed::Composed() :
+    rclcpp::Node("composed"),
+    _publisher(),
+    _timer(),  
+    _count(0)  
+{
+    //composed_publisherをコンストラクタで初期化した。
+    using namespace std::chrono_literals;
+    _publisher = this->create_publisher<std_msgs::msg::String>("/sensors_composed", 10);
+    composed_publisher = this->create_publisher<packet_interfaces::msg::Composed>("composed_topic", 10);
+
+    depth_subscription = this->create_subscription<packet_interfaces::msg::Depth>(
+        "depth_topic", 10, std::bind(&Composed::depth_topic_callback, this, std::placeholders::_1));
+
+    imu_subscription = this->create_subscription<sensor_msgs::msg::Imu>(
+        "imu_topic", 10, std::bind(&Composed::imu_topic_callback, this, std::placeholders::_1));
+
+    flex1_subscription = this->create_subscription<packet_interfaces::msg::Flex>(
+        "flex1_topic", 10, std::bind(&Composed::flex1_topic_callback, this, std::placeholders::_1));
+
+    flex2_subscription = this->create_subscription<packet_interfaces::msg::Flex>(
+        "flex2_topic", 10, std::bind(&Composed::flex2_topic_callback, this, std::placeholders::_1));
+
+    current_subscription = this->create_subscription<packet_interfaces::msg::Current>(
+        "current_topic", 10, std::bind(&Composed::current_topic_callback, this, std::placeholders::_1));
+
+    voltage_subscription = this->create_subscription<packet_interfaces::msg::Voltage>(
+        "voltage_topic", 10, std::bind(&Composed::voltage_topic_callback, this, std::placeholders::_1));
+   
+    auto loop = std::bind(&Composed::_loop, this);
+    _timer = this->create_wall_timer(500ms, loop);
+}
 
 void Composed::_loop() {
     std::stringstream ss;
@@ -14,15 +47,62 @@ void Composed::_loop() {
     _count++;
 }
 
-
-Composed::Composed() :
-    rclcpp::Node("composed"),
-    _publisher(),
-    _timer(),  
-    _count(0)  
+void Composed::depth_topic_callback(const packet_interfaces::msg::Depth& msg)
 {
-    using namespace std::chrono_literals;
-    _publisher = this->create_publisher<std_msgs::msg::String>("/sensors_composed", 10);
-    auto loop = std::bind(&Composed::_loop, this);
-    _timer = this->create_wall_timer(500ms, loop);
+    RCLCPP_INFO(this->get_logger(), "Received Depth message");
+    // Composedメッセージにデータを追加
+    auto composed_msg = packet_interfaces::msg::Composed();
+    composed_msg.depth = msg;
+    // 他のデータも追加する
+    composed_publisher->publish(composed_msg);
+}
+
+void Composed::imu_topic_callback(const sensor_msgs::msg::Imu& msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received IMU message");
+    // Composedメッセージにデータを追加
+    auto composed_msg = packet_interfaces::msg::Composed();
+    composed_msg.imu = msg;
+    // 他のデータも追加する
+    composed_publisher->publish(composed_msg);
+}
+
+void Composed::flex1_topic_callback(const packet_interfaces::msg::Flex1& msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received Flex1 message");
+    // Composedメッセージにデータを追加
+    auto composed_msg = packet_interfaces::msg::Composed();
+    composed_msg.flex1 = msg;
+    // 他のデータも追加する
+    composed_publisher->publish(composed_msg);
+}
+
+void Composed::flex2_topic_callback(const packet_interfaces::msg::Flex2& msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received Flex2 message");
+    // Composedメッセージにデータを追加
+    auto composed_msg = packet_interfaces::msg::Composed();
+    composed_msg.flex2 = msg;
+    // 他のデータも追加する
+    composed_publisher->publish(composed_msg);
+}
+
+void Composed::current_topic_callback(const packet_interfaces::msg::Current2& msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received Current message");
+    // Composedメッセージにデータを追加
+    auto composed_msg = packet_interfaces::msg::Composed();
+    composed_msg.Current = msg;
+    // 他のデータも追加する
+    composed_publisher->publish(composed_msg);
+}
+
+void Composed::voltage_topic_callback(const packet_interfaces::msg::Voltage& msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received Flex2 message");
+    // Composedメッセージにデータを追加
+    auto composed_msg = packet_interfaces::msg::Composed();
+    composed_msg.voltage = msg;
+    // 他のデータも追加する
+    composed_publisher->publish(composed_msg);
 }
