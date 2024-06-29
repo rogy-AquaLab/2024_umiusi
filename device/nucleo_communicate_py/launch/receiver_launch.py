f0d9fff22aba3e366b06318aba8973ea5a4d7667
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-06-30 00:28:14 +0900

    nucleo_communication.pyのlaunchファイルの修正

--- device/nucleo_communicate_py/launch/receiver_launch.py
+++ device/nucleo_communicate_py/launch/receiver_launch.py
@@ -6,6 +6,10 @@ def generate_launch_description() -> LaunchDescription:
     receiver = Node(
         package="nucleo_communicate_py",
         executable="receiver",
-        namespace="device"
+        namespace="device",
+        remappings=[("/device/receiver_launch", "/packet/sensor/flex/1"),
+                    ("/device/receiver_launch","/packet/sensor/flex/2"),
+                    ("/device/receiver_launch","/packet/sensor/current"),
+                    ("/device/receiver_launch","/packet/sensor/voltage")]
     )
     return LaunchDescription([receiver])
--- device/nucleo_communicate_py/launch/sender_launch.py
+++ device/nucleo_communicate_py/launch/sender_launch.py
@@ -6,6 +6,8 @@ def generate_launch_description() -> LaunchDescription:
     sender = Node(
         package="nucleo_communicate_py",
         executable="sender",
-        namespace="device"
+        namespace="device",
+        remappings=[("/device/sender_launch", "/packet/order/quit")
+                    ("/device/sender_launch","/packet/order/power")]
     )
     return LaunchDescription([sender])
