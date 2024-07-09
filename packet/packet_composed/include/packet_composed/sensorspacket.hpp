9e9a703d3269975ff126ddff388373980404266f
Noir-swim <noriri.jwu@gmail.com>
2024-07-09 23:28:31 +0900

    直せます。

--- packet/packet_composed/include/packet_composed/sensorspacket.hpp
+++ packet/packet_composed/include/packet_composed/sensorspacket.hpp
@@ -2,13 +2,19 @@
 #define PACKET_COMPOSED_SENSORSPACKET_HPP
 
 #include <memory>
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/string.hpp"
+#include "packet_interfaces/msg/composed.hpp"
+// #include "packet_interfaces/msg/depth.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+#include "packet_interfaces/msg/flex.hpp"
+#include "packet_interfaces/msg/current.hpp"
+#include "packet_interfaces/msg/voltage.hpp"
 
 class Composed : public rclcpp::Node {
 private:
     rclcpp::Publisher<packet_interfaces::msg::Composed>::SharedPtr composed_publisher;
-    rclcpp::Subscription<packet_interfaces::msg::Depth>::SharedPtr depth_subscription;
+    // rclcpp::Subscription<packet_interfaces::msg::Depth>::SharedPtr depth_subscription;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr         imu_subscription;
     rclcpp::Subscription<packet_interfaces::msg::Flex>::SharedPtr  flex1_subscription;
     rclcpp::Subscription<packet_interfaces::msg::Flex>::SharedPtr  flex2_subscription;
@@ -19,21 +25,21 @@ private:
     packet_interfaces::msg::Composed composed_msg;
 
     void _loop();
-    void depth_topic_callback(const packet_interfaces::msg::Depth& msg);
+    // void depth_topic_callback(const packet_interfaces::msg::Depth& msg);
     void imu_topic_callback(const sensor_msgs::msg::Imu& msg);
-    void flex1_topic_callback(const packet_interfaces::msg::Flex1& msg);
-    void flex2_topic_callback(const packet_interfaces::msg::Flex2& msg);
-    void current_topic_callback(const packet_interfaces::msg::current& msg);
-    void voltage_topic_callback(const packet_interfaces::msg::voltage& msg);
+    void flex1_topic_callback(const packet_interfaces::msg::Flex& msg);
+    void flex2_topic_callback(const packet_interfaces::msg::Flex& msg);
+    void current_topic_callback(const packet_interfaces::msg::Current& msg);
+    void voltage_topic_callback(const packet_interfaces::msg::Voltage& msg);
 
-    packet_interfaces::msg::Depth _depth;
+    // packet_interfaces::msg::Depth _depth;
     sensor_msgs::msg::Imu _imu;
     packet_interfaces::msg::Flex _flex1;
     packet_interfaces::msg::Flex _flex2;
     packet_interfaces::msg::Current _current;
     packet_interfaces::msg::Voltage _voltage;
 
-    bool _depth_received;
+    // bool _depth_received;
     bool _imu_received;
     bool _flex1_received;
     bool _flex2_received;
--- packet/packet_composed/src/composed.cpp
+++ packet/packet_composed/src/composed.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <functional>
+#include <packet_interfaces/msg/detail/flex__struct.hpp>
 #include <string>
 #include "packet_composed/sensorspacket.hpp"
 
@@ -7,13 +8,13 @@
 Composed::Composed() :
     rclcpp::Node("composed"),
     _timer(),
-    _depth(),
+    // _depth(),
     _imu(),
     _flex1(),
     _flex2(),
     _current(),
     _voltage(),
-    _depth_received(false),
+    // _depth_received(false),
     _imu_received(false),
     _flex1_received(false),
     _flex2_received(false),
@@ -24,8 +25,8 @@ Composed::Composed() :
     using namespace std::chrono_literals;
     composed_publisher = this->create_publisher<packet_interfaces::msg::Composed>("sensors_composed", 10);
 
-    depth_subscription = this->create_subscription<packet_interfaces::msg::Depth>(
-        "sensors/depth", 10, std::bind(&Composed::depth_topic_callback, this, std::placeholders::_1));
+    // depth_subscription = this->create_subscription<packet_interfaces::msg::Depth>(
+    //     "sensors/depth", 10, std::bind(&Composed::depth_topic_callback, this, std::placeholders::_1));
 
     imu_subscription = this->create_subscription<sensor_msgs::msg::Imu>(
         "sensors/imu", 10, std::bind(&Composed::imu_topic_callback, this, std::placeholders::_1));
@@ -48,10 +49,12 @@ Composed::Composed() :
 
 void Composed::_loop() {
 
-    if (_depth_received && _imu_received && _flex1_received && _flex2_received && _current_received && _voltage_received) 
+// add _depth_received
+
+    if (_imu_received && _flex1_received && _flex2_received && _current_received && _voltage_received) 
     {
         auto composed_msg = packet_interfaces::msg::Composed();
-        composed_msg.depth = _depth;
+        // composed_msg.depth = _depth;
         composed_msg.imu = _imu;
         composed_msg.flex1 = _flex1;
         composed_msg.flex2 = _flex2;
@@ -63,13 +66,13 @@ void Composed::_loop() {
     }
 }
 
-void Composed::depth_topic_callback(const packet_interfaces::msg::Depth& msg)
-{
-    RCLCPP_DEBUG(this->get_logger(), "Received Depth message");
-    auto composed_msg = packet_interfaces::msg::Composed();
-    _depth = msg;
-    _depth_received = true;
-}
+// void Composed::depth_topic_callback(const packet_interfaces::msg::Depth& msg)
+// {
+//     RCLCPP_DEBUG(this->get_logger(), "Received Depth message");
+//     auto composed_msg = packet_interfaces::msg::Composed();
+//     _depth = msg;
+//     _depth_received = true;
+// }
 
 void Composed::imu_topic_callback(const sensor_msgs::msg::Imu& msg)
 {
@@ -78,21 +81,21 @@ void Composed::imu_topic_callback(const sensor_msgs::msg::Imu& msg)
     _imu_received = true;
 }
 
-void Composed::flex1_topic_callback(const packet_interfaces::msg::Flex1& msg)
+void Composed::flex1_topic_callback(const packet_interfaces::msg::Flex& msg)
 {
     RCLCPP_DEBUG(this->get_logger(), "Received Flex1 message");
     _flex1 = msg;
     _flex1_received = true; 
 }
 
-void Composed::flex2_topic_callback(const packet_interfaces::msg::Flex2& msg)
+void Composed::flex2_topic_callback(const packet_interfaces::msg::Flex& msg)
 {
     RCLCPP_DEBUG(this->get_logger(), "Received Flex2 message");
     _flex2 = msg;
     _flex2_received = true;
 }
 
-void Composed::current_topic_callback(const packet_interfaces::msg::Current2& msg)
+void Composed::current_topic_callback(const packet_interfaces::msg::Current& msg)
 {
     RCLCPP_DEBUG(this->get_logger(), "Received Current message");
     _current = msg;
