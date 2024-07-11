f75a69a87b552b86804deda7d4bb42c4784c842e
H1rono <hronok66@gmail.com>
2024-07-12 03:44:27 +0900

    Take lock

--- device/pi_i2c/pi_i2c/imu.py
+++ device/pi_i2c/pi_i2c/imu.py
@@ -1,4 +1,5 @@
 import sys
+from threading import Lock
 
 import rclpy
 import smbus2
@@ -17,20 +18,22 @@ class Imu(Node):
         self._imu_publisher = self.create_publisher(ImuMsg, "imu", 10)
         self._reset_subscription = self.create_subscription(Empty, "imu/reset", self._reset_callback, 10)
         self._timer = self.create_timer(0.5, self._timer_callback)
+        self._bno055_lock = Lock()
 
         # https://github.com/H1rono/rpi-bno055/blob/c6516b1920a9d31582977eb1c31f03e68bcf6a5e/rpi_bno055/scripts.py#L115-L128
         self._bno055.begin()
         self._reset_bno055()
 
     def _reset_bno055(self) -> None:
-        self._bno055.system_trigger(BNO055.SysTriggerFlag.RST_SYS)
-        # accelerometer: m/s^2
-        self._bno055.update_unit_selection(BNO055.UnitSelection.ACC_MPS2)
-        # gyroscope: rad/s
-        self._bno055.update_unit_selection(BNO055.UnitSelection.GYR_RPS)
-        # euler: radians
-        self._bno055.update_unit_selection(BNO055.UnitSelection.EUL_RADIANS)
-        self._bno055.write_mode(BNO055.modes.IMU)
+        with self._bno055_lock:
+            self._bno055.system_trigger(BNO055.SysTriggerFlag.RST_SYS)
+            # accelerometer: m/s^2
+            self._bno055.update_unit_selection(BNO055.UnitSelection.ACC_MPS2)
+            # gyroscope: rad/s
+            self._bno055.update_unit_selection(BNO055.UnitSelection.GYR_RPS)
+            # euler: radians
+            self._bno055.update_unit_selection(BNO055.UnitSelection.EUL_RADIANS)
+            self._bno055.write_mode(BNO055.modes.IMU)
         self.get_logger().info("Performed bno055 restart")
 
     def _reset_callback(self, _msg: Empty) -> None:
@@ -41,19 +44,20 @@ class Imu(Node):
         # IMUからデータを取得してpublish
         self.get_logger().debug("tick")
         try:
-            # https://github.com/H1rono/rpi-bno055/blob/c6516b1920a9d31582977eb1c31f03e68bcf6a5e/rpi_bno055/scripts.py#L131-L134
-            linear_accel = self._bno055.read_linear_accel()
-            gravity = self._bno055.read_gravity()
-            quaternion = self._bno055.read_quaternion()
-            angular_velocity = self._bno055.read_gyroscope()
-            self.get_logger().info(f"gravity: {gravity}")
-            # TODO: covariance の計算
-            msg = ImuMsg()
-            msg.header.frame_id = "bno055"
-            msg.orientation = encode_quat(quaternion)
-            msg.angular_velocity = encode_vec3(angular_velocity)
-            msg.linear_acceleration = encode_vec3(linear_accel)
-            self._imu_publisher.publish(msg)
+            with self._bno055_lock:
+                # https://github.com/H1rono/rpi-bno055/blob/c6516b1920a9d31582977eb1c31f03e68bcf6a5e/rpi_bno055/scripts.py#L131-L134
+                linear_accel = self._bno055.read_linear_accel()
+                gravity = self._bno055.read_gravity()
+                quaternion = self._bno055.read_quaternion()
+                angular_velocity = self._bno055.read_gyroscope()
+                self.get_logger().info(f"gravity: {gravity}")
+                # TODO: covariance の計算
+                msg = ImuMsg()
+                msg.header.frame_id = "bno055"
+                msg.orientation = encode_quat(quaternion)
+                msg.angular_velocity = encode_vec3(angular_velocity)
+                msg.linear_acceleration = encode_vec3(linear_accel)
+                self._imu_publisher.publish(msg)
         except OSError:
             self.get_logger().error("Failed to read data from IMU")
 
