cd667d20275e9ea5a097b1f7be2faf2fb34c2ce2
H1rono <hronok66@gmail.com>
2024-07-11 22:28:35 +0900

    Add reset endpoint

--- device/pi_i2c/pi_i2c/imu.py
+++ device/pi_i2c/pi_i2c/imu.py
@@ -5,6 +5,7 @@ import smbus2
 from rclpy.node import Node
 from rpi_bno055 import BNO055
 from sensor_msgs.msg import Imu as ImuMsg
+from std_msgs.msg import Empty
 
 from .utils import encode_quat, encode_vec3
 
@@ -14,10 +15,14 @@ class Imu(Node):
         super().__init__("imu")
         self._bno055 = BNO055(bus=bus)
         self._imu_publisher = self.create_publisher(ImuMsg, "imu", 10)
+        self._reset_subscription = self.create_subscription(Empty, "imu/reset", self._reset_callback, 10)
         self._timer = self.create_timer(0.5, self._timer_callback)
 
         # https://github.com/H1rono/rpi-bno055/blob/c6516b1920a9d31582977eb1c31f03e68bcf6a5e/rpi_bno055/scripts.py#L115-L128
         self._bno055.begin()
+        self._reset_bno055()
+
+    def _reset_bno055(self) -> None:
         self._bno055.system_trigger(BNO055.SysTriggerFlag.RST_SYS)
         # accelerometer: m/s^2
         self._bno055.update_unit_selection(BNO055.UnitSelection.ACC_MPS2)
@@ -26,6 +31,10 @@ class Imu(Node):
         # euler: radians
         self._bno055.update_unit_selection(BNO055.UnitSelection.EUL_RADIANS)
         self._bno055.write_mode(BNO055.modes.IMU)
+        self.get_logger().info("Performed bno055 restart")
+
+    def _reset_callback(self, _msg: Empty) -> None:
+        self._reset_bno055()
 
     def _timer_callback(self) -> None:
         # TODO
