b603d6857055f4c01bea0953772f6cae0868c333
H1rono <hronok66@gmail.com>
2024-07-12 19:14:31 +0900

    Fix setup.py

--- device/camera_reader/setup.py
+++ device/camera_reader/setup.py
@@ -13,7 +13,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         (
-            os.path.join("share", "package_name", "launch"),
+            os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*")),
         ),
     ],
