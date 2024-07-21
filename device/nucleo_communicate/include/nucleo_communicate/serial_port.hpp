bf98de9743f88aeec2deb863f6ddbd492e0d4717
H1rono <hronok66@gmail.com>
2024-07-22 02:17:19 +0900

    Add `SerialPort` class

---
+++ device/nucleo_communicate/include/nucleo_communicate/serial_port.hpp
@@ -0,0 +1,34 @@
+#ifndef NUCLEO_COMMUNICATE_SERIAL_PORT_HPP
+#define NUCLEO_COMMUNICATE_SERIAL_PORT_HPP
+
+#include <stdio.h>
+#include <string>
+#include <termios.h>
+
+#include "nucleo_communicate/recv_data.hpp"
+#include "nucleo_communicate/send_data.hpp"
+
+namespace nucleo_com {
+
+class SerialPort {
+private:
+    FILE* serial;
+
+public:
+    explicit SerialPort(const std::string& path = "/dev/ttyACM0");
+
+    ~SerialPort();
+
+    // same as
+    // https://os.mbed.com/docs/mbed-os/v6.16/apis/serial-uart-apis.html#configuration
+    void setup();
+
+    // https://github.com/rogy-AquaLab/2024_umiusi_nucleo
+    void send(const nucleo_com::SendData& data);
+
+    auto receive() -> nucleo_com::RecvData;
+};
+
+}
+
+#endif // NUCLEO_COMMUNICATE_SERIAL_PORT_HPP
---
+++ device/nucleo_communicate/src/serial_port.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <utility>
+
+#include "nucleo_communicate/recv_data.hpp"
+#include "nucleo_communicate/send_data.hpp"
+#include "nucleo_communicate/serial_port.hpp"
+
+using nucleo_com::SerialPort;
+
+SerialPort::SerialPort(const std::string& path) : serial(nullptr) {
+    this->serial = fopen(path.c_str(), "r+");
+}
+
+SerialPort::~SerialPort() {
+    fclose(this->serial);
+}
+
+void SerialPort::setup() {
+    // 9600-8-N-1
+    // baud rate: 9600
+    // data frame: 8
+    // no parity bit
+    // stop bit: 1
+    termios tio{};
+    tio.c_cflag = CREAD | CLOCAL | CS8;
+    cfsetspeed(&tio, B9600);
+    cfmakeraw(&tio);
+    const int fd = fileno(this->serial);
+    tcsetattr(fd, TCSANOW, &tio);
+    ioctl(fd, TCSETS, &tio);
+}
+
+void SerialPort::send(const SendData& data) {
+    std::uint8_t header = 0x00;
+    fwrite(&header, sizeof(std::uint8_t), 1, this->serial);
+    fwrite(
+        data.buffer.data(),
+        sizeof(SendData::BufferType::value_type),
+        data.buffer.size(),
+        this->serial
+    );
+}
+
+auto SerialPort::receive() -> RecvData {
+    std::uint8_t header = 0x01;
+    fwrite(&header, sizeof(std::uint8_t), 1, this->serial);
+    RecvData::BufferType buffer{};
+    fread(
+        buffer.data(),
+        sizeof(RecvData::BufferType::value_type),
+        buffer.size(),
+        this->serial
+    );
+    return RecvData(std::move(buffer));
+}
