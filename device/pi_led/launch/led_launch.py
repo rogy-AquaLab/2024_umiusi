a0c4128b559eec8cc9e434acdf9e0b6352c2e840
23-saho <sahonorth@gmail.com>
2024-07-28 00:37:34 +0900

    parameter

--- device/pi_led/launch/led_launch.py
+++ device/pi_led/launch/led_launch.py
@@ -1,12 +1,20 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
+    config = os.path.join(
+        get_package_share_directory("pi_led"),
+        "config",
+        "default_param.yml",
+    )
     led = Node(
         package="pi_led",
         excutable="pi_led",
         namespace="device",
-        parameters=[],
+        parameters=[config],
     )
     return LaunchDescription([led])
