05b644932f9321bcf9cace185fc955d836f5e929
Noir-swim <noriri.jwu@gmail.com>
2024-08-01 20:44:16 +0900

    遅くなりましたm(_ _)m 修正しました。

--- app/trapezoidal_acc/include/trapezoidal_acc/trapezoid.hpp
+++ app/trapezoidal_acc/include/trapezoidal_acc/trapezoid.hpp
@@ -13,11 +13,11 @@ public:
     Trapezoid();
 
 private:
-    rclcpp::Publisher<power_map_msg::msg::NormalizedPower>::SharedPtr Trapezoid_publisher;
-    rclcpp::Subscription<power_map_msg::msg::NormalizedPower>::SharedPtr Trapezoid_subscription;
+    rclcpp::Publisher<power_map_msg::msg::NormalizedPower>::SharedPtr trapezoid_publisher;
+    rclcpp::Subscription<power_map_msg::msg::NormalizedPower>::SharedPtr trapezoid_subscription;
 
     rclcpp::TimerBase::SharedPtr _timer;
-    power_map_msg::msg::NormalizedPower NormalizedPower_msg;
+    power_map_msg::msg::NormalizedPower normalizedPower_msg;
 
     void _loop();
     void trapezoid_topic_callback(const power_map_msg::msg::NormalizedPower& msg);
@@ -25,7 +25,7 @@ private:
     double _current_velocity;
     double _target_velocity;
 
-    power_map_msg::msg::NormalizedPower _NormalizedPower;
+    power_map_msg::msg::NormalizedPower _normalizedPower;
 
 };
 
--- app/trapezoidal_acc/src/trapezoid.cpp
+++ app/trapezoidal_acc/src/trapezoid.cpp
@@ -5,13 +5,13 @@
 #include "trapezoidal_acc/trapezoid.hpp"
 
 Trapezoid::Trapezoid(): 
-  rclcpp::Node("trapezoid_acc"),
-  _current_velocity(0.0), //現在の速度（初期化）
-  _target_velocity(10.0)// 目標速度
+    rclcpp::Node("trapezoid_acc"),
+    _current_velocity(0.0), //現在の速度（初期化）
+    _target_velocity(10.0)// 目標速度
 {
-    Trapezoid_publisher = this->create_publisher<power_map_msg::msg::NormalizedPower>("/app/unsafe_power", 10);
-    Trapezoid_subscription = this->create_subscription<power_map_msg::msg::NormalizedPower>(
-        "/app/safe_power", 10, std::bind(&Trapezoid::trapezoid_topic_callback, this, std::placeholders::_1));
+    trapezoid_publisher = this->create_publisher<power_map_msg::msg::NormalizedPower>("/app/unsafe_power", 10);
+    trapezoid_subscription = this->create_subscription<power_map_msg::msg::NormalizedPower>(
+        "safe_power", 10, std::bind(&Trapezoid::trapezoid_topic_callback, this, std::placeholders::_1));
 
     _timer = this->create_wall_timer(
         std::chrono::milliseconds(100), std::bind(&Trapezoid::_loop, this));
@@ -19,19 +19,20 @@ Trapezoid::Trapezoid():
 
 void Trapezoid::trapezoid_topic_callback(const power_map_msg::msg::NormalizedPower& msg)
 {
-  RCLCPP_DEBUG(this->get_logger(), "Received trapezoid_message");
-  _NormalizedPower= msg;
+    RCLCPP_DEBUG(this->get_logger(), "Received trapezoid_message");
+    _normalizedPower= msg;
 }
 
 void Trapezoid::_loop() //速度のアルゴリズムがあまり理解できていないです。。
 {
-  double threshold_velocity = 5.0; // 速度の限度（仮に5.0とする。）
+    double threshold_velocity = 5.0; // 速度の限度（仮に5.0とする。）
 
   // 速度がしきい値を超えた場合に速度を変更
-  if (_current_velocity > threshold_velocity) {
-    _current_velocity = threshold_velocity;
-  } 
+    if (_current_velocity > threshold_velocity) 
+    {
+        _current_velocity = threshold_velocity;
+    } 
 
-  auto message = power_map_msg::msg::NormalizedPower();
-  Trapezoid_publisher->publish(message);
+    auto message = power_map_msg::msg::NormalizedPower();
+    trapezoid_publisher->publish(message);
 }
