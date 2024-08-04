8a52de243a89af5753679ebb947d7ded9704e27f
H1rono <hronok66@gmail.com>
2024-08-05 01:58:59 +0900

    Add parameter `param_file` in power_map_launch.py

--- app/power_map/launch/power_map_launch.py
+++ app/power_map/launch/power_map_launch.py
@@ -2,22 +2,26 @@ import os.path
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    config = os.path.join(
+    default_param_file = os.path.join(
         get_package_share_directory("power_map"),
         "config",
         "default_param.yml",
     )
+    param_file_arg = DeclareLaunchArgument("param_file", default_value=default_param_file)
     return LaunchDescription(
         [
+            param_file_arg,
             Node(
                 package="power_map",
                 executable="power-map",
                 namespace="app",
-                parameters=[config],
+                parameters=[LaunchConfiguration("param_file")],
                 remappings=[("/app/power", "/device/order/power")],
             ),
         ],
