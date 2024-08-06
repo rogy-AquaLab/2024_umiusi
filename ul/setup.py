5b55b024148582f44d773b2efe601ec95d154efa
H1rono <hronok66@gmail.com>
2024-08-06 17:46:51 +0900

    Add launch argument in `ul/simple_joy_app_pc.py`

--- ul/setup.py
+++ ul/setup.py
@@ -16,6 +16,10 @@ setup(
             os.path.join("share", package_name, "launch"),
             glob(os.path.join("launch", "*")),
         ),
+        (
+            os.path.join("share", package_name, "config"),
+            glob(os.path.join("config", "*")),
+        ),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
