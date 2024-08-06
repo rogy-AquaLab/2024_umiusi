576ac3a4655302756ca18bf188d36b031cdcc417
H1rono <hronok66@gmail.com>
2024-08-07 02:09:40 +0900

    Add `log_level`

--- app/imshow/launch/imshow_launch.py
+++ app/imshow/launch/imshow_launch.py
@@ -16,17 +16,23 @@ def imshow_node(**kwargs: Any) -> Node:
 
 def generate_launch_description() -> LaunchDescription:
     index_arg = DeclareLaunchArgument("index", default_value="-1")
+    log_level_arg = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        choices=["debug", "info", "warn", "error", "fatal"],
+    )
     index = LaunchConfiguration("index")
+    log_level = LaunchConfiguration("log_level")
     index_specified = PythonExpression([index, ">= 0"])
     indexed_imshow = imshow_node(
         name=["imshow_", index],
         remappings=[("/app/camera_image", ["/packet/camera_image_", index])],
+        ros_arguments=["--log-level", log_level],
         condition=IfCondition(index_specified),
     )
     unindexed_imshow = imshow_node(
         remappings=[("/app/camera_image", "/packet/camera_image")],
+        ros_arguments=["--log-level", log_level],
         condition=UnlessCondition(index_specified),
     )
-    return LaunchDescription(
-        [index_arg, indexed_imshow, unindexed_imshow],
-    )
+    return LaunchDescription([index_arg, log_level_arg, indexed_imshow, unindexed_imshow])
--- app/power_map/launch/power_map_launch.py
+++ app/power_map/launch/power_map_launch.py
@@ -10,15 +10,18 @@ def generate_launch_description():
         [FindPackageShare("power_map"), "config", "default_param.yml"]
     )
     param_file_arg = DeclareLaunchArgument("param_file", default_value=param_file)
-    return LaunchDescription(
-        [
-            param_file_arg,
-            Node(
-                package="power_map",
-                executable="power-map",
-                namespace="app",
-                parameters=[LaunchConfiguration("param_file")],
-                remappings=[("/app/power", "/device/order/power")],
-            ),
-        ],
+    log_level_arg = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        choices=["debug", "info", "warn", "error", "fatal"],
     )
+    log_level = LaunchConfiguration("log_level")
+    power_map = Node(
+        package="power_map",
+        executable="power-map",
+        namespace="app",
+        parameters=[LaunchConfiguration("param_file")],
+        remappings=[("/app/power", "/device/order/power")],
+        ros_arguments=["--log-level", log_level],
+    )
+    return LaunchDescription([param_file_arg, log_level_arg, power_map])
