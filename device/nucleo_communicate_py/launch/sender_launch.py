2e8fcf679c332af12253bc9e6100d2207ba1c9d2
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-08-10 23:37:55 +0900

    sender

--- device/nucleo_communicate_py/launch/sender_launch.py
+++ device/nucleo_communicate_py/launch/sender_launch.py
@@ -1,8 +1,16 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
+    log_level_arg = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        choices=["debug", "info", "warn", "error", "fatal"],
+    )
+    log_level = LaunchConfiguration("log_level")
     sender = Node(
         package="nucleo_communicate_py",
         executable="sender",
@@ -11,5 +19,6 @@ def generate_launch_description() -> LaunchDescription:
             ("/device/quit", "/packet/order/quit"),
             ("/device/order/power", "/packet/order/power"),
         ],
+        ros_arguments=["--log-level", log_level],
     )
-    return LaunchDescription([sender])
+    return LaunchDescription([log_level_arg,sender])
