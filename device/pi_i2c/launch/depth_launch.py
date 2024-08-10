1f1dc31ba0951b8b6d278d2b7e906fcefae1396e
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-08-10 23:38:08 +0900

    depth

--- device/pi_i2c/launch/depth_launch.py
+++ device/pi_i2c/launch/depth_launch.py
@@ -1,11 +1,20 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
+    log_level_arg = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        choices=["debug", "info", "warn", "error", "fatal"],
+    )
+    log_level = LaunchConfiguration("log_level")
     depth = Node(
         package="pi_i2c",
         executable="depth",
         namespace="device",
+        ros_arguments=["--log-level",log_level],
     )
-    return LaunchDescription([depth])
+    return LaunchDescription([log_level_arg,depth])
