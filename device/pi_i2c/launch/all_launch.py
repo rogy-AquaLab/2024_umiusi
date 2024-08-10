4a42e2d228f2084234bba2c0772944ea05f255af
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-08-10 23:38:01 +0900

    all_launch

--- device/pi_i2c/launch/all_launch.py
+++ device/pi_i2c/launch/all_launch.py
@@ -1,12 +1,21 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 
 def generate_launch_description() -> LaunchDescription:
+    log_level_arg = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        choices=["debug", "info", "warn", "error", "fatal"],
+    )
+    log_level = LaunchConfiguration("log_level")
     all_exec = Node(
         package="pi_i2c",
         executable="all",
         namespace="device",
         remappings=[("/device/imu", "/packet/sensors/imu")],
+        ros_arguments=["--log-level",log_level]
     )
-    return LaunchDescription([all_exec])
+    return LaunchDescription([log_level_arg,all_exec])
