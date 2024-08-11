d91333ceb86922d4cb7e777e72e7fb0048d1af50
H1rono <hronok66@gmail.com>
2024-08-11 17:59:34 +0900

    Update nucleo_communicate channel_launch.py

--- device/nucleo_communicate/launch/channel_launch.py
+++ device/nucleo_communicate/launch/channel_launch.py
@@ -8,12 +8,14 @@ def generate_launch_description():
         executable="nucleo-channel",
         namespace="device",
         remappings=[
+            ("/device/nucleo_state", "/packet/nucleo_state"),
             ("/device/flex_1", "/packet/sensors/flex_1"),
             ("/device/flex_2", "/packet/sensors/flex_2"),
             ("/device/current", "/packet/sensors/current"),
             ("/device/voltage", "/packet/sensors/voltage"),
             ("/device/power", "/packet/order/power"),
-            ("/device/quit", "/packet/order/quit"),
+            ("/device/initialize", "/packet/order/initialize"),
+            ("/device/suspend", "/packet/order/suspend"),
         ],
     )
     return LaunchDescription([channel])
