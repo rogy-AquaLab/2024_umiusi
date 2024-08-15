1da380c4ed29be392350d4c865a4f4bda79feace
23-yoshikawa <yoshikawa.h.ah@m.titech.ac.jp>
2024-08-15 11:46:47 +0900

    Revert "py>pi"
    This reverts commit 7291cc83eccf226b414fb1a978ae162d0cebc86f.

--- app/imshow/imshow/imshow.py
+++ app/imshow/imshow/imshow.py
@@ -3,20 +3,17 @@ import numpy as np
 import rclpy
 from cv_bridge import CvBridge
 from rclpy.node import Node
-from rclpy.qos import HistoryPolicy, QoSProfile, ReliabilityPolicy
 from sensor_msgs.msg import CompressedImage
 
 
 class Imshow(Node):
     def __init__(self):
         super().__init__("imshow")
-        qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT,
-                                 history=HistoryPolicy.KEEP_LAST,depth=10)
         self.subscription = self.create_subscription(
             CompressedImage,
             "camera_image",
             self.listener_callback,
-            qos_profile,
+            10,
         )
         self.bridge = CvBridge()
 
